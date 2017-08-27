ef10dc3380427e6064818f7966a4add454ee3570
thanhthinhcntt <thanhthinhcntt@gmail.com>
2017-08-27 21:22:02 +0700

    add-ng-class

---
+++ Angular/Angular/ng-class/js/controller/MovesController.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    function MoviesController() {
+        this.favorites = [{
+            title: 'The Shawshank Redemption',
+            year: '1994',
+            popular: true,
+            category:'A'
+        }, {
+            title: 'Inception',
+            year: '2010',
+            popular: false,
+            category: 'B'
+        }, {
+            title: 'The Matrix',
+            year: '1999',
+            popular: false,
+            category: 'C'
+        }, {
+            title: 'Saving Private Ryan',
+            year: '1998',
+            popular: false,
+            category: 'D'
+        }];
+    }
+
+    angular
+        .module('app')
+        .controller('MoviesController', MoviesController);
+})();
